4f3b6598deda5608b1a02bc8a940c71bf295c9ba
mohamedyasser27 <my2722000@gmail.com>
2022-10-27 19:15:12 +0200

    Make the transition smoother ont the side menu
    i converted from using width which triggers Layout,paint,composition
    to using transfrom:scale which only triggers composition

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let openMenuBtn = document.querySelector(".openMenuBtn");
 let closeMenuBtn = document.querySelector(".closeMenuBtn");
 let sideMenu = document.querySelector(".sideMenu");
+let cover = document.querySelector(".cover");
 closeMenuBtn.addEventListener("click", () => {
   sideMenu.classList.toggle("invisible");
+  cover.classList.toggle("invisible");
+
 });
 openMenuBtn.addEventListener("click", () => {
   sideMenu.classList.toggle("invisible");
+  cover.classList.toggle("invisible");
+
 });
 
 let collapsedBtns = Array.from(document.querySelectorAll(".collapsedBtn"));
+
 collapsedBtns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     event.target.classList.toggle("expanded");
